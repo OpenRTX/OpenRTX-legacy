46a29c8b896ee215dd5246f6b80372809b81e26d
Silvano Seva <silseva@fastwebnet.it>
2020-03-31 21:52:26 +0200

    Checking if external clock is OK

--- source/usb_test.c
+++ source/usb_test.c
@@ -10,7 +10,9 @@ int main (void)
 {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;
     GPIOE->MODER |= (1 << 2) | 1;
-    GPIOE->ODR ^= (1 << 0);
+
+    if(RCC->CR & RCC_CR_HSERDY) GPIOE->ODR ^= (1 << 0);
+    if((RCC->CFGR & RCC_CFGR_SWS ) == RCC_CFGR_SWS_PLL) GPIOE->ODR ^= (1 << 1);
 
     TM_USB_VCP_Init();
     uint8_t c;
@@ -19,7 +21,6 @@ int main (void)
     {
         if (TM_USB_VCP_GetStatus() == TM_USB_VCP_CONNECTED)
         {
-            GPIOE->ODR ^= (1 << 0);
             if (TM_USB_VCP_Getc(&c) == TM_USB_VCP_DATA_OK)
             {
                 TM_USB_VCP_Putc(c);
@@ -27,7 +28,6 @@ int main (void)
         }
         else
         {
-            GPIOE->ODR ^= (1 << 1);
         }
 
         vTaskDelay(50);
