96a38d9360a07cb33899df2104afae97fc0c224a
Silvano Seva <silseva@fastwebnet.it>
2020-04-02 14:58:53 +0200

    Functions for interrupt handlers all back to __attribute__((weak)), changed main() body for USB test

--- source/usb_test.c
+++ source/usb_test.c
@@ -3,31 +3,18 @@
 #include <string.h>
 #include "stm32f4xx.h"
 #include "usb_vcp.h"
+#include "usb_bsp.h"
 
 int main (void)
 {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;
     GPIOE->MODER |= (1 << 2) | 1;
 
-//     if(RCC->CR & RCC_CR_HSERDY) GPIOE->ODR ^= (1 << 0);
-//     if((RCC->CFGR & RCC_CFGR_SWS ) == RCC_CFGR_SWS_PLL) GPIOE->ODR ^= (1 << 1);
-
     TM_USB_VCP_Init();
-    uint8_t c;
 
     while (1)
     {
-        if (TM_USB_VCP_GetStatus() == TM_USB_VCP_CONNECTED)
-        {
-            if (TM_USB_VCP_Getc(&c) == TM_USB_VCP_DATA_OK)
-            {
-                TM_USB_VCP_Putc(c);
-            }
-        }
-        else
-        {
-        }
-
-//         vTaskDelay(50);
+        TM_USB_VCP_Puts("Test\r\n\0");
+        USB_OTG_BSP_mDelay(500);
     }
 }
