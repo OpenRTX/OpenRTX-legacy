55c108f1ee8667cc0d0ec42e6ddf4ca485ed8e8f
Silvano Seva <silseva@fastwebnet.it>
2020-04-07 10:32:43 +0200

    Making malloc_test compile and work

--- source/testsuites/malloc_test.c
+++ source/testsuites/malloc_test.c
@@ -1,47 +1,56 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "FreeRTOS.h"
 #include "stm32f4xx.h"
 #include "gpio.h"
 
-void uDelay (const uint32_t usec)
-{
-    uint32_t count = 0;
-    const uint32_t utime = (120 * usec / 7);
-
-    do
-    {
-        if( ++count > utime )
-        {
-            return ;
-        }
-    } while (1);
-}
-
-void testAllocation(size_t size)
-{
-    printf("Allocating a chunk of %zd bytes... ");
-    uint8_t *p = ((uint8_t *) malloc(size));
-    if(p != NULL)
-    {
-        puts("OK");
-        free(p);
-    }
-    else
-    {
-        puts("FAIL");
-    }
-}
-
-int main (void)
+void testAllocation(void *arg)
 {
     size_t chunk = 8;
     while(chunk <= 4096)
     {
-        testAllocation(chunk);
+        printf("Allocating a chunk of %zd bytes... ", chunk);
+        uint8_t *p = ((uint8_t *) malloc(chunk));
+        if(p != NULL)
+        {
+            puts("OK");
+            free(p);
+        }
+        else
+        {
+            puts("FAIL");
+        }
         chunk *= 2;
+        vTaskDelay(250);
     }
 
     puts("End of test");
     for(;;) ;
 }
+
+void blink(void *arg)
+{
+    while(1)
+    {
+        gpio_togglePin(GPIOE, 0);
+        vTaskDelay(500);
+    }
+}
+
+int main (void)
+{
+    gpio_setMode(GPIOE, 0, OUTPUT);
+
+    xTaskCreate(testAllocation, "testAllocation", 256, NULL, 1, NULL);
+    xTaskCreate(blink, "blink", 256, NULL, 0, NULL);
+    vTaskStartScheduler();
+
+    for(;;) ;
+}
+
+void vApplicationTickHook() { }
+void vApplicationStackOverflowHook() { }
+void vApplicationIdleHook() { }
+void vApplicationMallocFailedHook() { }
