91fd90b4fbe11f5afc385050a995c068774284b2
Silvano Seva <silseva@fastwebnet.it>
2020-04-11 18:49:45 +0200

    Trying again with FSMC for display driving

--- drivers/stm32f4xx/lcd.c
+++ drivers/stm32f4xx/lcd.c
@@ -109,6 +109,9 @@
 #define RENDER_WR_SETUP_DELAY 30
 #define RENDER_NEXT_BYTE_DELAY 30
 
+#define LCD_FSMC_ADDR_COMMAND 0x60000000
+#define LCD_FSMC_ADDR_DATA    0x60040000 
+
 /*
  * LCD framebuffer, allocated on the heap by lcd_init().
  * Pixel format is RGB565, 16 bit per pixel
@@ -343,6 +346,70 @@ void lcd_init()
     writeCmd(CMD_RAMWR);
 
     gpio_setPin(CS);
+
+    /* Configure FSMC as LCD driver.
+     * BCR1 config:
+     * - CBURSTRW  = 0: asynchronous write operation
+     * - ASYNCWAIT = 0: NWAIT not taken into account when running asynchronous protocol
+     * - EXTMOD    = 0: do not take into account values of BWTR register
+     * - WAITEN    = 0: nwait signal disabled
+     * - WREN      = 1: write operations enabled
+     * - WAITCFG   = 0: nwait active one data cycle before wait state
+     * - WRAPMOD   = 0: direct wrapped burst disabled
+     * - WAITPOL   = 0: nwait active low
+     * - BURSTEN   = 0: burst mode disabled
+     * - FACCEN    = 1: NOR flash memory disabled
+     * - MWID      = 1: 16 bit external memory device
+     * - MTYP      = 2: NOR
+     * - MUXEN     = 0: addr/data not multiplexed
+     * - MBNEN     = 1: enable bank
+     */
+    RCC->AHB3ENR |= RCC_AHB3ENR_FSMCEN;
+    FSMC_Bank1->BTCR[0] = 0x10D9;
+                        //= FSMC_BCR1_EXTMOD
+                        //| FSMC_BCR1_WREN
+                        //| FSMC_BCR1_MBKEN;
+
+    /* BTR1 config:
+     * - ACCMOD  = 0: access mode A
+     * - DATLAT  = 0: don't care in asynchronous mode
+     * - CLKDIV  = 1: don't care in asynchronous mode, 0000 is reserved
+     * - BUSTURN = 0: time between two consecutive write accesses: (1 + 2 + BUSTURN)*HCLK_period = 71.4ns > twc (66ns)
+     * - DATAST  = 3: we must have LCD twrl < DATAST*HCLK_period: 15ns < 3*5.95 = 17.85ns
+     * - ADDHLD  = 1: used only in mode D, 0000 is reserved
+     * - ADDSET  = 1: address setup time 3*HCLK_period = 17.85ns
+     */
+    FSMC_Bank1->BTCR[1] = (0 << 28) /* ACCMOD */
+                        | (0 << 24) /* DATLAT */
+                        | (1 << 20) /* CLKDIV */
+                        | (0 << 16) /* BUSTURN */
+                        | (5 << 8)  /* DATAST */
+                        | (1 << 4)  /* ADDHLD */
+                        | 7;        /* ADDSET */
+
+    gpio_setMode(D0, ALTERNATE);
+    gpio_setMode(D1, ALTERNATE);
+    gpio_setMode(D2, ALTERNATE);
+    gpio_setMode(D3, ALTERNATE);
+    gpio_setMode(D4, ALTERNATE);
+    gpio_setMode(D5, ALTERNATE);
+    gpio_setMode(D6, ALTERNATE);
+    gpio_setMode(D7, ALTERNATE);
+    gpio_setMode(RS, ALTERNATE);
+    gpio_setMode(WR, ALTERNATE);
+    gpio_setMode(RD, ALTERNATE);
+
+    gpio_setAlternateFunction(D0, 12);
+    gpio_setAlternateFunction(D1, 12);
+    gpio_setAlternateFunction(D2, 12);
+    gpio_setAlternateFunction(D3, 12);
+    gpio_setAlternateFunction(D4, 12);
+    gpio_setAlternateFunction(D5, 12);
+    gpio_setAlternateFunction(D6, 12);
+    gpio_setAlternateFunction(D7, 12);
+    gpio_setAlternateFunction(RS, 12);
+    gpio_setAlternateFunction(WR, 12);
+    gpio_setAlternateFunction(RD, 12);
 }
 
 void lcd_terminate()
@@ -366,17 +433,18 @@ void lcd_render()
      * Put screen data lines back to output mode, since they are in common with
      * keyboard buttons and the keyboard driver sets them as inputs.
      */
-    gpio_setMode(D0, OUTPUT);
-    gpio_setMode(D1, OUTPUT);
-    gpio_setMode(D2, OUTPUT);
-    gpio_setMode(D3, OUTPUT);
-    gpio_setMode(D4, OUTPUT);
-    gpio_setMode(D5, OUTPUT);
-    gpio_setMode(D6, OUTPUT);
-    gpio_setMode(D7, OUTPUT);
+//     gpio_setMode(D0, OUTPUT);
+//     gpio_setMode(D1, OUTPUT);
+//     gpio_setMode(D2, OUTPUT);
+//     gpio_setMode(D3, OUTPUT);
+//     gpio_setMode(D4, OUTPUT);
+//     gpio_setMode(D5, OUTPUT);
+//     gpio_setMode(D6, OUTPUT);
+//     gpio_setMode(D7, OUTPUT);
 
     gpio_clearPin(CS);
-    writeCmd(CMD_RAMWR);
+//     writeCmd(CMD_RAMWR);
+    *(volatile uint8_t*)LCD_FSMC_ADDR_COMMAND = CMD_RAMWR;
 
     /*
      * Copying data from framebuffer to screen buffer. When using the 8-bit bus
@@ -396,25 +464,28 @@ void lcd_render()
         uint16_t rg = (frameBuffer[p] >> 8) & 0xFF; /* red and half green  */
         uint16_t gb = frameBuffer[p] & 0xFF;        /* half green and blue */
 
-        /* Send red and half green */
-        GPIOD->BSRRH = 0xC023;                      /* Clear D0, D1, D2, D3, WR */
-        GPIOE->BSRRH = 0x0780;                      /* Clear D4, D5, D6, D7 */
-        GPIOD->BSRRL = ((rg << 14) & 0xC000)        /* Set D0, D1 */
-                        | ((rg >> 2) & 0x0003);     /* D2, D3 */
-        GPIOE->BSRRL = (rg << 3) & 0x0780;          /* Set D4, D5, D6, D7 */
-        delayUs(RENDER_WR_SETUP_DELAY);
-        GPIOD->BSRRL = (1 << 5);                    /* Set WR line */
-        delayUs(RENDER_NEXT_BYTE_DELAY);
-
-        /* Send remaining half green and blue */
-        GPIOD->BSRRH = 0xC023;                      /* Clear D0, D1, D2, D3, WR */
-        GPIOE->BSRRH = 0x0780;                      /* Clear D4, D5, D6, D7 */
-        GPIOD->BSRRL = ((gb << 14) & 0xC000)        /* Set D0, D1 */
-                        | ((gb >> 2) & 0x0003);     /* D2, D3 */
-        GPIOE->BSRRL = (gb << 3) & 0x0780;          /* Set D4, D5, D6, D7 */
-        delayUs(RENDER_WR_SETUP_DELAY);
-        GPIOD->BSRRL = (1 << 5);                    /* Set WR line */
-        delayUs(RENDER_NEXT_BYTE_DELAY);
+        *(volatile uint8_t*)LCD_FSMC_ADDR_DATA = ((uint8_t) rg);
+        *(volatile uint8_t*)LCD_FSMC_ADDR_DATA = ((uint8_t) gb);
+        
+//         /* Send red and half green */
+//         GPIOD->BSRRH = 0xC023;                      /* Clear D0, D1, D2, D3, WR */
+//         GPIOE->BSRRH = 0x0780;                      /* Clear D4, D5, D6, D7 */
+//         GPIOD->BSRRL = ((rg << 14) & 0xC000)        /* Set D0, D1 */
+//                         | ((rg >> 2) & 0x0003);     /* D2, D3 */
+//         GPIOE->BSRRL = (rg << 3) & 0x0780;          /* Set D4, D5, D6, D7 */
+//         delayUs(RENDER_WR_SETUP_DELAY);
+//         GPIOD->BSRRL = (1 << 5);                    /* Set WR line */
+//         delayUs(RENDER_NEXT_BYTE_DELAY);
+// 
+//         /* Send remaining half green and blue */
+//         GPIOD->BSRRH = 0xC023;                      /* Clear D0, D1, D2, D3, WR */
+//         GPIOE->BSRRH = 0x0780;                      /* Clear D4, D5, D6, D7 */
+//         GPIOD->BSRRL = ((gb << 14) & 0xC000)        /* Set D0, D1 */
+//                         | ((gb >> 2) & 0x0003);     /* D2, D3 */
+//         GPIOE->BSRRL = (gb << 3) & 0x0780;          /* Set D4, D5, D6, D7 */
+//         delayUs(RENDER_WR_SETUP_DELAY);
+//         GPIOD->BSRRL = (1 << 5);                    /* Set WR line */
+//         delayUs(RENDER_NEXT_BYTE_DELAY);
     }
 
     gpio_setPin(CS);
